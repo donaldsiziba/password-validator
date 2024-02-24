3020a245e8abd9f5a2a43a0b3dbb18bbc1384bdd
donald <donald@awesomatic.co.za>
2024-02-24 10:03:09 +0200

    improve syntax to initialize rule list

--- src/main/java/za/co/awesomatic/tdd/PasswordValidator.java
+++ src/main/java/za/co/awesomatic/tdd/PasswordValidator.java
@@ -10,15 +10,15 @@ import java.util.function.Function;
 import static java.util.stream.Collectors.toCollection;
 
 public class PasswordValidator implements Function<PasswordValidationRequest, PasswordValidationResponse> {
-    private List<ValidationRule> rules = new ArrayList<ValidationRule>() {{
-        add(new RegexValidationRule(".*[A-Z].*", "The password should have at least one uppercase character"));
-        add(new RegexValidationRule(".*[a-z].*", "The password should have at least one lowercase character"));
-        add(new RegexValidationRule(".*[0-9].*", "The password should have at least one digit"));
-        add(new RegexValidationRule(".*[^a-zA-Z0-9].*", "The password should have at least one special character"));
-        add(new RegexValidationRule(".{12,}", "The password should be at least 12 characters long"));
-        add(new RegexValidationRule("\\S+$", "The password should not have any whitespaces"));
-        add(new PasswordHistoryValidationRule("The password should not match any of the previous passwords"));
-    }};
+    private final List<ValidationRule> rules = List.of(
+        new RegexValidationRule(".*[A-Z].*", "The password should have at least one uppercase character"),
+        new RegexValidationRule(".*[a-z].*", "The password should have at least one lowercase character"),
+        new RegexValidationRule(".*[0-9].*", "The password should have at least one digit"),
+        new RegexValidationRule(".*[^a-zA-Z0-9].*", "The password should have at least one special character"),
+        new RegexValidationRule(".{12,}", "The password should be at least 12 characters long"),
+        new RegexValidationRule("\\S+$", "The password should not have any whitespaces"),
+        new PasswordHistoryValidationRule("The password should not match any of the previous passwords")
+    );
 
     @Override
     public PasswordValidationResponse apply(PasswordValidationRequest request) {
